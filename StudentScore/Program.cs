d637313077ee1cfa48c7a85ca360356f2f1f865c
knowledge <knowledge@Tshimologong.joburg>
2020-02-18 08:53:48 +0200

    First

---
+++ StudentScore/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace StudentScore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StudentScoreClass objStudent = new StudentScoreClass();
+           
+                Console.WriteLine($"Enter Your Score for student:");
+
+            int score = int.Parse(Console.ReadLine());
+            objStudent.myCalculation(score);
+
+            objStudent.display();
+
+            //foreach (var item in arScore)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+           
+
+
+
+        }
+    }
+}
---
+++ StudentScore/StudentScoreClass.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentScore
+{
+    class StudentScoreClass
+    {
+        public int Score { get; set; }
+        public int[] arScore = new int[3];
+        public int highest = 0;
+        public int lowest = 0;
+        public int aver = 0;
+        public void stu
+        public void myCalculation(int score)
+        {
+
+           
+            for (int j = 0; j < 3; j++)
+            {
+              if (Score > 0 & Score < 100)
+                {
+                    arScore[j] = Score;
+                    for (int i = 1; i < arScore.Length; i++)
+                    {
+                        int pos = 0;
+                        if (arScore[i] > arScore[pos])
+                        {
+                            highest = arScore[i];
+                        }
+                        else
+                        {
+                            lowest = arScore[i];
+                        }
+                    }
+
+
+                }
+                else
+                {
+                    Console.WriteLine($"Score must be between 0-100 not{Score}");
+                }
+                aver += arScore[j] / arScore.Length;
+            }
+           
+        }
+        public void display()
+        {
+            Console.WriteLine($"Highest Score Is:{highest}");
+            Console.WriteLine($"Lowest Score Is:{lowest}");
+            Console.WriteLine($"average Score Is:{aver}");
+        }
+    }
+}
